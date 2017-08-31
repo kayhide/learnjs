76b5247c23984b2775d1b5dbbd410f9bfdca4f6d
kayhide <kayhide@gmail.com>
2017-08-31 20:19:43 +0900

    add view parameter.

--- public/app.js
+++ public/app.js
@@ -1,7 +1,17 @@
 'use strict';
 var learnjs = {};
 
+learnjs.problemView = function(problemNumber) {
+  return $('<div class="problem-view">').text('Problem #' + problemNumber + ' Coming soon!');
+}
+
 learnjs.showView = function(hash) {
-  var problemView = $('<div class="problem-view">').text('Coming soon!');
-  $('.view-container').empty().append(problemView);
+  var routes = {
+    '#problem': learnjs.problemView
+  };
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]));
+  }
 }
